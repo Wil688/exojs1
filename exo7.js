08210c76c894b3726ef997d71e31f5c562d42829
Wil688 <wilfriedkouame@hotmail.fr>
2021-05-21 14:31:10 +0200

    exo with Amy

--- exo7.js
+++ exo7.js
@@ -1,11 +1,11 @@
 
 function showResult() {
-   var shoes=document.getElementById('shoeSize').value;
-   var birth=document.getElementById('yearOfBirth').value;
-   var shoes=parseInt(shoes);
-   var birth=parseInt(birth);
+   let shoes = document.getElementById('shoeSize').value;
+   let birth = document.getElementById('yearOfBirth').value;
+   let shoes = parseInt(shoes);
+   let birth = parseInt(birth);
    
-   var result= ((((shoes* 2 + 5) * 50) - birth) + 1766);
+   let result= ((((shoes* 2 + 5) * 50) - birth) + 1766);
    alert(result);
 }
 document.getElementById("submit").addEventListener("click",showResult);
